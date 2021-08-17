f35500fc8051305433db060d48985f42b7a0cec3
Maximous Black <maximousblk@gmail.com>
2021-08-17 12:05:43 +0000

    don't use .env file

--- dump.sh
+++ dump.sh
@@ -1,11 +1,12 @@
+docker pull postgres:alpine
+
 echo "creating dump..."
 docker run \
-  --env-file .env \
   --env PGPASSWORD \
   --env PGHOST \
   --env PGUSER=postgres \
-  postgres pg_dump -O -x --clean --if-exists -Z 9 \
-  -n extensions \
+  postgres:alpine pg_dump -O -x --clean --if-exists -Z 9 \
   -n public >nest_dev.sql.gz
 echo "dump created:"
+
 ls -lh nest_dev.sql.gz
