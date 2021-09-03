82427a4e8d05743f75c4c43f277064b533a64f4b
Maximous Black <maximousblk@gmail.com>
2021-09-03 14:03:11 +0000

    refactor: use local db to create the dump

--- dump.sh
+++ dump.sh
@@ -1,12 +1,25 @@
-docker pull postgres:alpine
+docker run -d --rm \
+  --name db_dump \
+  --publish 5432:5432 \
+  --env POSTGRES_PASSWORD=postgres \
+  --env POSTGRES_USER=postgres \
+  --env POSTGRES_DB=postgres \
+  postgres:alpine
 
-echo "creating dump..."
-docker run \
-  --env PGPASSWORD \
-  --env PGHOST \
+sleep 30
+
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5432/postgres?schema=public"
+
+yarn push
+yarn seed
+
+docker exec \
+  --env PGHOST=localhost \
   --env PGUSER=postgres \
-  postgres:alpine pg_dump -O -x --clean --if-exists -Z 9 \
-  -n public >nest_dev.sql.gz
-echo "dump created:"
+  --env PGPASSWORD=postgres \
+  db_dump pg_dump -O -x --clean --if-exists -Z 9 \
+  -n public >nest_db.sql.gz
 
-ls -lh nest_dev.sql.gz
+ls -lh nest_db.sql.gz
+
+docker stop db_dump
